670ec5beee1b73d75ba32d96d76626a0cd1cf7de
rvaquerizo4 <rvaqueri@jaumebalmes.net>
2022-11-10 20:36:10 +0100

    CalcularEdad hecho

--- CalcularEdad.py
+++ CalcularEdad.py
@@ -1,8 +1,10 @@
+#Importo date de datetime, que es una clase creada ya de python
 from datetime import date 
-  
-def calcularEdad(cumpleaños): 
-    days_in_year = 365.2425    
-    edat = int((date.today() - cumpleaños).days / days_in_year) 
-    return edat 
-          
-print(calcularEdad(date(2003, 11, 25)), "años") 
+#declaro la funcion que le pasamos una fecha y te dice tu edad
+def calcularEdad(cumpleaños):
+    #declaro variables necessarias
+    año = 365.2425    
+    edat = int((date.today() - cumpleaños).days / año) 
+    return edat
+#Hacemos un print que te indicara tu edad
+print("Usted tiene "+str(calcularEdad(date(2003, 11, 25))), "años") 
