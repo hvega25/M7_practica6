a8190802667ae6ba2bfb60f8e7ff1fd58499067d
4Tipy <lchavez21@jaumebalmes.net>
2022-11-10 19:48:05 +0100

    mi practica

---
+++ conc.py
@@ -0,0 +1,35 @@
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    # Actividad4
+
+    # Variables
+
+    x = 3
+    y = 4
+    name = "leo"
+    name2 = "steven"
+
+    # Prints
+
+    print(x + y)
+    print(name + " " + name2)
+    print(f"La suma de x e y es: " + str(x + y))
+
+    # Actividad 6
+
+
+def actividad6(valor1, valor2):
+    print(f'Hi, {actividad6()}')
+
+
+if __name__ == '__main__':
+    valor1 = int(input('Coloca un numero: '))
+    valor2 = int(input('Coloca otro numero: '))
+    suma = valor1 + valor2
+
+    print(f"La suma del valor '{valor1}' mas el valor '{valor2}' es: " + str(suma))
+
